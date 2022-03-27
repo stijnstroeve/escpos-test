ab43f7f20df77d3f50af57975c314cbae2240d18
luki <szymon@storekeeper.nl>
2022-03-27 12:59:06 +0200

    Added REGO filtering

--- index.js
+++ index.js
@@ -5,23 +5,33 @@ const findBySerialNumber = async (serialNumber) => {
     const devices = getDeviceList();
     const opened = (device) => !!device.interfaces;
 
+    const regoVendor = 1155; // Vendor=0483 (hex)
     for (const device of devices) {
         try {
+            const {idVendor}  = device.deviceDescriptor;
+            if( idVendor !== regoVendor){
+                continue; // not REGO
+            }
+            const {busNumber,deviceAddress}  = device;
+            console.log('Found Rego', {busNumber,deviceAddress});
             try {
                 if (!opened(device)) {
                     device.open();
                 }
             } catch(e) {
                 // Ignore any errors, device may be a system device or inaccessible
+                console.error('device open, fail', e)
                 continue;
             }
 
             const getStringDescriptor = util.promisify(device.getStringDescriptor).bind(device);
             let buffer = await getStringDescriptor(device.deviceDescriptor.iSerialNumber);
 
-            buffer = buffer.replace(/\0.*$/g,''); // Only change compared to the on from the library.
-            if (buffer && buffer.toString() === serialNumber) {
-                return device;
+            if( buffer ){
+                buffer = buffer.replace(/\0.*$/g,''); // Only change compared to the on from the library.
+                if (buffer && buffer.toString() === serialNumber) {
+                    return device;
+                }
             }
         } finally {
             try {
